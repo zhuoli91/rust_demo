bbad4e31795d6115945ce413917c2688ed1ce304
zli <30396815+zhuoli91@users.noreply.github.com>
2022-07-20 18:41:12 +0800

    update

--- src/sum_calculator/sum_calculator.rs
+++ src/sum_calculator/sum_calculator.rs
@@ -1,12 +1,12 @@
 fn calculate_sum(arr: &[u32]) -> Option<u32> {
-    let mut sum: Option<u32> = Some(0);
+    let mut sum: u32 = 0;
     for i in arr.iter() {
-        sum = match sum {
-            Some(s) => s.checked_add(*i),
-            None => None,
-        };
+        match sum.checked_add(*i) {
+            Some(s) => sum = s,
+            None => return None,
+        }
     }
-    sum
+    Some(sum)
 }
 
 pub fn run() {
@@ -14,7 +14,7 @@ pub fn run() {
     let sum1 = calculate_sum(&arr1);
     println!("array: {:?}\nsum: {:?}\n", arr1, sum1);
 
-    let arr2: [u32; 3] = [1, 2, u32::MAX];
+    let arr2: [u32; 3] = [1, u32::MAX, 3];
     let sum2 = calculate_sum(&arr2);
     println!("array: {:?}\nsum: {:?}\n", arr2, sum2);
 }
